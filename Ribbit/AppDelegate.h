4ecdabd35e79883ebd748273562f7d3df5dc5dbe
ltfschoen <ltfschoen@gmail.com>
2014-05-08 21:59:04 +1000

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Luke on 8/05/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
