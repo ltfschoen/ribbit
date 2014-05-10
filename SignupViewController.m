3edc0077b24447fc8e700e45f070e968b4adef02
ltfschoen <ltfschoen@gmail.com>
2014-05-10 13:00:26 +1000

    Additional Comments added to Sign Up to explain Asynchronous back-end Multi-threading and Callback mechanism Block

--- SignupViewController.m
+++ SignupViewController.m
@@ -53,13 +53,16 @@
         // refer to documentation at https://www.parse.com/docs/ios_guide#users/iOS
         PFUser *newUser = [PFUser user];
         
-        // set required username and password values of PFUser with values stored above
+        // set required username and password properties of PFUser with values
+        // stored above as entered from the Storyboard TextFields
         newUser.username = username;
         newUser.password = password;
         newUser.email = email;
         
-        // save data to Parse.com by calling save method
+        // asynchronous back-end transaction saves data to Parse.com by calling save method
         [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            // run the following when back-end transaction complete and response routed
+            // into this block to be handled as successful creation or error
             // check if error variable is set to null (perceived as false/NO)
             if (error) {
                 // handle error using UIAlertView
