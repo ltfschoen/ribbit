76e50a4c75f25ccef31d6ef560594263bbc81f62
ltfschoen <ltfschoen@gmail.com>
2014-05-11 15:41:35 +1000

    Created Edit Friends Scene and Custom Class. Connected Scene to Class

---
+++ Ribbit/EditFriendsViewController.m
@@ -0,0 +1,50 @@
+//
+//  EditFriendsViewController.m
+//  Ribbit
+//
+//  Created by Luke on 11/05/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import "EditFriendsViewController.h"
+
+@interface EditFriendsViewController ()
+
+@end
+
+@implementation EditFriendsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
