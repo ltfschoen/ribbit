66d4cf6b4a7d7797a872c3adac9e9b4d7fdcb8df
ltfschoen <ltfschoen@gmail.com>
2014-05-19 20:44:01 +1000

    Added CameraViewController Custom Class

---
+++ Ribbit/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  Ribbit
+//
+//  Created by Luke on 19/05/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UITableViewController
+
+@end
